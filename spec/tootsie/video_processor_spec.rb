889a533fa821dcf70aa40f0497c9508e91e35ba1
Arseniy Ivanov <freeatnet@freeatnet.com>
2016-02-22 15:26:47 -0500

    [#1] Improving video transcoder tests

--- spec/tootsie/video_processor_spec.rb
+++ spec/tootsie/video_processor_spec.rb
@@ -7,37 +7,23 @@ include Tootsie::Processors
 
 describe VideoProcessor do
 
-  it "performs basic ffmpeg transcoding" do
-    stub_request(:get, "http://example.com/video.mp4").
-      to_return(:status => 200, :body => 'garblegarblegarble')
+  describe "#execute!" do
+    let(:input_url) { "http://example.com/video.mp4" }
+    let(:input_data) { "garblegarblegarble" + rand(0..1000).to_s }
+    let(:output_url) { "http://example.com/video.flv" }
+    let(:output_data) { "DATA" + rand(0..1000).to_s }
 
-    expect_any_instance_of(FfmpegAdapter).to receive(:transcode).
-      with(kind_of(String), kind_of(String), kind_of(Hash)).
-      once { |_, in_path, out_path, adapter_options|
-      expect(adapter_options).to include(
-        :audio_sample_rate => 44100,
-        :audio_bitrate => 64000,
-        :format => "flv",
-        :width => 600,
-        :height => 400,
-        :quality => 1.0,
-        :content_type => "video/x-flv")
-      File.open(out_path, "w") { |f|
-        f.write("DATA")
-      }
-    }
+    subject { VideoProcessor.new(processor_params) }
 
-    post_stub = stub_request(:post, "http://example.com/video.flv").
-      with(
-        :headers => {'Content-Type' => 'video/x-flv'},
-        :body => 'DATA').
-      to_return(:status => 200, :body => 'garblegarblegarble')
+    before(:each) do
+      stub_request(:get, input_url).
+        to_return(:status => 200, :body => input_data)
+    end
 
-    VideoProcessor.new(
-      :input_url => "http://example.com/video.mp4",
-      :versions => [
+    context "with video transcoding requested" do
+      let (:version_options) do
         {
-          :target_url => "http://example.com/video.flv",
+          :target_url => output_url,
           :audio_sample_rate => 44100,
           :audio_bitrate => 64000,
           :format => 'flv',
@@ -46,9 +32,114 @@ describe VideoProcessor do
           :quality => 1.0,
           :content_type => "video/x-flv"
         }
-      ]).execute!
+      end
 
-    expect(post_stub).to have_been_requested
+      let!(:post_stub) do
+        stub_request(:post, output_url).with(
+          :headers => {'Content-Type' => version_options[:content_type]},
+          :body => output_data
+        ).to_return(:status => 200)
+      end
+
+      before(:each) do
+        expect_any_instance_of(FfmpegAdapter).to receive(:transcode).
+          with(kind_of(String), kind_of(String), kind_of(Hash)).
+          once { |_, in_path, out_path, adapter_options|
+          expect(adapter_options).to include(version_options.except(:target_url))
+          File.open(out_path, "w") { |f|
+            f.write(output_data)
+          }
+        }
+      end
+
+      context "versions as a hash" do
+        let(:processor_params) do
+          {
+            :input_url => input_url,
+            :versions => version_options
+          }
+        end
+
+        it "performs basic transcoding" do
+          subject.execute!
+          expect(post_stub).to have_been_requested
+        end
+      end
+
+      context "versions as an array" do
+        let(:processor_params) do
+          {
+            :input_url => input_url,
+            :versions => [version_options, ]
+          }
+        end
+
+        it "performs basic transcoding" do
+          subject.execute!
+          expect(post_stub).to have_been_requested
+        end
+      end
+    end
+
+    context "with video transcoding and a thumbnail requested" do
+      let(:thumbnail_target_url) { "http://example.com/video.jpg" }
+      let(:thumbnail_options) do
+        {
+          :target_url => thumbnail_target_url,
+          :width => 600,
+          :height => 400
+        }
+      end
+      let(:version_options) do
+        {
+          :target_url => output_url,
+          :audio_sample_rate => 44100,
+          :audio_bitrate => 64000,
+          :format => 'flv',
+          :width => 600,
+          :height => 400,
+          :quality => 1.0,
+          :content_type => "video/x-flv"
+        }
+      end
+
+      let!(:version_post_stub) do
+        stub_request(:post, output_url).with(
+          :headers => {'Content-Type' => version_options[:content_type]},
+          :body => output_data
+        ).to_return(:status => 200)
+      end
+
+      let!(:thumbnail_post_stub) do
+        # TODO: Verify headers and data for the thumbnail
+        stub_request(:post, thumbnail_target_url).
+          to_return(:status => 200)
+      end
+
+      before(:each) do
+        expect_any_instance_of(FfmpegAdapter).to receive(:transcode).
+          with(kind_of(String), kind_of(String), kind_of(Hash)).
+          once { |_, in_path, out_path, adapter_options|
+          expect(adapter_options).to include(version_options.except(:target_url))
+          File.open(out_path, "w") { |f|
+            f.write(output_data)
+          }
+        }
+      end
+
+      let(:processor_params) do
+        {
+          :input_url => input_url,
+          :thumbnail => thumbnail_options,
+          :versions => [ version_options, ]
+        }
+      end
+
+      it "performs basic transcoding and creates a thumbnail" do
+        subject.execute!
+        expect(version_post_stub).to have_been_requested
+        expect(thumbnail_post_stub).to have_been_requested
+      end
+    end
   end
-
 end
